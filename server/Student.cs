657ca91305230e921ccba9f98719976722294820
NechamiBM <n7110315@gmail.com>
2024-02-13 18:19:00 +0200

    מסודר

--- server/Student.cs
+++ server/Student.cs
@@ -9,8 +9,8 @@
         public string address { get; set; }
         public string phone { get; set; }
         public bool isActive { get; set; }
-        public int avgMarks { get; set; }
         public int courseId { get; set; }
+        public int avgMarks { get; set; }
         public DateTime lastDay { get; set; }
         public int year { get; set; }
     }
